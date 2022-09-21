4a8e63d10bb24af9e78491978b949b49f3f9c7aa
InaDjn <inayath.djany@gmail.com>
2022-09-21 21:32:25 +0200

    first commit

--- app.py
+++ app.py
@@ -89,7 +89,10 @@ def predict_homecredit(data: HomeCredit):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    #uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='p7-fastapi.herokuapp.com')
+
+    
 
 # uvicorn app:app --reload
 # uvicorn main:app --reload
